new configs, fix visualization tag, move scale factor computation

dino_eval.py
@@ -105,8 +105,10 @@ def eval_accuracy(model: nn.Module, dataloader: DataLoader, writer: SummaryWrite
 
         if i % vis_every_n_batch == 0:
             batch_im_paths = [dataloader.dataset.samples[idx][0] for idx in sample_indices.tolist()]
-            visualize_topk_prototypes(outputs, batch_im_paths, writer, i, epoch_name="eval")
-            visualize_prototype_assignments(outputs, labels, writer, i, epoch_name="eval", figsize=(10, 10,))
+            visualize_topk_prototypes(outputs, batch_im_paths, writer,
+                                      tag_fmt_str="Evaluation batch {step} top{topk} prototype assignment/{idx}", step=i)
+            visualize_prototype_assignments(outputs, labels, writer, step=i,
+                                            fmt_str=f"Evaluation batch {i} prototype assignments")
 
         mca_eval(outputs["class_logits"], labels)
 

models/dino.py
@@ -143,8 +143,6 @@ class ProtoDINO(nn.Module):
             patch_prototype_logits = rearrange(patch_prototype_logits, "B n_patches (C K) -> B n_patches C K", C=self.C, K=self.n_prototypes)
         else:
             patch_prototype_logits = einsum(patch_tokens_norm, prototype_norm, "B n_patches dim, C K dim -> B n_patches C K")
-        
-        patch_prototype_logits = self.scale * patch_prototype_logits
 
         if self.pooling_method == "max":
             image_prototype_logits, _ = patch_prototype_logits.max(1)  # shape: [B, C, K,], C=n_classes+1
@@ -155,7 +153,7 @@ class ProtoDINO(nn.Module):
 
         if self.cls_head == "sa" and self.sa is not None:
             sa_weights = F.softmax(self.sa, dim=-1) * self.n_prototypes
-            image_prototype_logits_weighted = image_prototype_logits[:, :-1, :] * sa_weights
+            image_prototype_logits_weighted = self.scale * image_prototype_logits[:, :-1, :] * sa_weights
             class_logits = image_prototype_logits_weighted.sum(-1)
         elif self.cls_head == "fc" and self.fc is not None:
             image_prototype_logits_flat = rearrange(image_prototype_logits[:, :-1, :], "B n_classes K -> B (n_classes K)")

train_dino.py
@@ -48,8 +48,10 @@ def train_epoch(model: nn.Module, criterion: nn.Module | None, dataloader: DataL
 
         if debug and i == 0:
             batch_im_paths = [dataloader.dataset.samples[idx][0] for idx in sample_indices.tolist()]
-            visualize_topk_prototypes(outputs, batch_im_paths, writer, epoch, epoch_name="train")
-            visualize_prototype_assignments(outputs, labels, writer, epoch, epoch_name="train", figsize=(8, 10,))
+            visualize_topk_prototypes(outputs, batch_im_paths, writer, step=epoch,
+                                      tag_fmt_str="Training epoch {step} batch 0 top{topk} prototypes/{idx}")
+            visualize_prototype_assignments(outputs, labels, writer, step=epoch,
+                                            tag=f"Training epoch {epoch} batch {i} prototype assignments")
 
     for k, v in running_losses.items():
         loss_avg = v / len(dataloader.dataset)
@@ -74,9 +76,10 @@ def val_epoch(model: nn.Module, dataloader: DataLoader, epoch: int, writer: Summ
 
         if debug and i == 0:
             batch_im_paths = [dataloader.dataset.samples[idx][0] for idx in sample_indices.tolist()]
-            visualize_topk_prototypes(outputs, batch_im_paths, writer, epoch, epoch_name="val")
-            visualize_prototype_assignments(outputs, labels, writer, epoch, epoch_name="val", figsize=(10, 10,))
-
+            visualize_topk_prototypes(outputs, batch_im_paths, writer, step=epoch,
+                                      tag_fmt_str="Validation epoch {step} batch 0 top{topk} prototypes/{idx}")
+            visualize_prototype_assignments(outputs, labels, writer, step=epoch,
+                                            tag=f"Validation epoch {epoch} batch {i} prototype assignments")
         mca_val(outputs["class_logits"], labels)
 
     epoch_acc_val = mca_val.compute().item()

utils/visualization.py
@@ -29,8 +29,8 @@ def overlay_attn_map(attn_map: np.ndarray, im: Image.Image):
 def visualize_topk_prototypes(batch_outputs: dict[str, torch.Tensor],
                               batch_im_paths: list[str],
                               writer: SummaryWriter,
-                              epoch: int,
-                              epoch_name: str,
+                              tag_fmt_str: str,
+                              step: int,
                               *,
                               topk: int = 5,
                               input_size: int = 224,
@@ -73,18 +73,20 @@ def visualize_topk_prototypes(batch_outputs: dict[str, torch.Tensor],
         plt.close(fig=fig)
         
         figures.append(fig_image)
-        writer.add_image(f"Epoch {epoch} top {topk} prototypes/{epoch_name}/{b}", F.pil_to_tensor(fig_image))
+
+        fmt_items = dict(step=step, topk=topk, idx=b)
+        writer.add_image(tag_fmt_str.format(**fmt_items), F.pil_to_tensor(fig_image))
 
     return figures
 
 
 def visualize_prototype_assignments(outputs: dict[str, torch.Tensor], labels: torch.Tensor, writer: SummaryWriter,
-                                    step: int, epoch_name: str, figsize: tuple[int, int] = (8, 10,)):
+                                    tag: str, step: int, figsize: tuple[int, int] = (10, 12,)):
     patch_labels = outputs["pseudo_patch_labels"].detach().clone()  # shape: [B, H, W,]
     L_c_dict = {c: L_c.detach().clone() for c, L_c in outputs["L_c_dict"].items()}
 
     nrows, ncols = figsize
-    fig, axes = plt.subplots(nrows, ncols, figsize=(10, 10,))
+    fig, axes = plt.subplots(nrows, ncols, figsize=(ncols, nrows,))
 
     for b, (c, ax) in enumerate(zip(labels.cpu().tolist(), axes.flat)):
         patch_labels_b = patch_labels[b, :, :]  # shape: [H, W,], dtype: torch.long
@@ -108,7 +110,7 @@ def visualize_prototype_assignments(outputs: dict[str, torch.Tensor], labels: to
     fig_image = Image.frombuffer('RGBa', fig.canvas.get_width_height(), fig.canvas.buffer_rgba()).convert("RGB")
     plt.close(fig=fig)
     
-    writer.add_image(f"Batch prototype assignment/{epoch_name}", F.pil_to_tensor(fig_image), global_step=step)
+    writer.add_image(tag, F.pil_to_tensor(fig_image), global_step=step)
     
     return fig_image
 