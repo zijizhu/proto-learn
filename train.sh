722123555f69755382fa36e71ae85163d59352d9
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-08-28 13:07:39 +1000

    bug fix

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@
 
 set -x
 
-# python train_dino.py --config_path experiments_new/vitb14-n_splits_3-new.yaml
-# python dino_eval.py --log_dir logs_latest/vitb14-n_splits_3-new.yaml
-#
+python train_dino.py --config_path experiments_new/vitb14-n_splits_3-new.yaml
+python dino_eval.py --log_dir logs_latest/vitb14-n_splits_3-new.yaml
+
 python train_dino.py --config_path experiments_new/vits14-n_splits_3-new.yaml
 python dino_eval.py --log_dir logs_latest/vits14-n_splits_3-new.yaml
 
