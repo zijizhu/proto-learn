efd92c50043a8407b2ad2ea686e8c1a851e7c8f5
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-09-02 22:25:26 +1000

    add new experiment configs

--- train.sh
+++ train.sh
@@ -14,5 +14,20 @@ python dino_eval.py --log_dir logs/dinov2_vits14-base
 python train_dino.py --config_path experiments/dinov2_vits14-fine_tuned.yaml
 python dino_eval.py --log_dir logs/dinov2_vits14-fine_tuned
 
+python train_dino.py --config_path e./experiments_new/dinov2_vitb14-lr_0.01.yaml
+python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.01
+
+python train_dino.py --config_path e./experiments_new/dinov2_vitb14-lr_0.005.yaml
+python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.005
+
+python train_dino.py --config_path e./experiments_new/dinov2_vitb14-lr_0.008.yaml
+python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.008
+
+python train_dino.py --config_path e./experiments_new/dinov2_vitb14-scale_lr_0.001.yaml
+python dino_eval.py --log_dir logs/dinov2_vitb14-scale_lr_0.001
+
+python train_dino.py --config_path e./experiments_new/dinov2_vits14-scale_lr_0.0001.yaml
+python dino_eval.py --log_dir logs/dinov2_vits14-scale_lr_0.0001
+
 python train_dino.py --config_path experiments/clip_vitb16-base.yaml
 python dino_eval.py --log_dir logs_latest/clip_vitb16-base
