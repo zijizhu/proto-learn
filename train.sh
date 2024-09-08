2e3aabcf4660079d13dcbe23b7c96144b451c50d
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-09-09 00:36:17 +1000

    update training script

--- train.sh
+++ train.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
 
-set -x
+declare -a filenames=(
+"dinov2_vitb14-fg_resnet18_0.4-n_proto_5-all_losses-l_orth_coef_0.001"
+"dinov2_vitb14-fg_resnet18_0.4-n_proto_5-all_losses"
+"dinov2_vitb14-fg_resnet18_0.4-n_proto_6-all_losses-l_orth_coef_0.001"
+"dinov2_vitb14-fg_resnet18_0.4-n_proto_6-all_losses-l_orth_coef_0.003"
+"dinov2_vitb14-fg_resnet18_0.4-n_proto_6-all_losses"
+)
 
-python dino_eval.py --log_dir logs/dinov2_vitb14-base
-
-python dino_eval.py --log_dir logs/dinov2_vitb14-fine_tuned
-
-python dino_eval.py --log_dir logs/dinov2_vits14-base
-
-python dino_eval.py --log_dir logs/dinov2_vits14-fine_tuned
-
-python train_dino.py --config_path experiments_new/dinov2_vitb14-lr_0.01.yaml
-python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.01
-
-python train_dino.py --config_path experiments_new/dinov2_vitb14-lr_0.005.yaml
-python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.005
-
-python train_dino.py --config_path experiments_new/dinov2_vitb14-lr_0.008.yaml
-python dino_eval.py --log_dir logs/dinov2_vitb14-lr_0.008
-
-python train_dino.py --config_path experiments_new/dinov2_vitb14-scale_lr_0.001.yaml
-python dino_eval.py --log_dir logs/dinov2_vitb14-scale_lr_0.001
-
-python train_dino.py --config_path experiments_new/dinov2_vits14-scale_lr_0.0001.yaml
-python dino_eval.py --log_dir logs/dinov2_vits14-scale_lr_0.0001
-
-python train_dino.py --config_path experiments/clip_vitb16-base.yaml
-python dino_eval.py --log_dir logs/clip_vitb16-base
+for f in "${filenames[@]}"
+do
+  python train_dino.py --config_path "experiments_new/$f.yaml"
+  python dino_eval.py --log_dir "logs/$f"
+done
