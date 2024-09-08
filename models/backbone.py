d2b523df26a7c32bd13a7420586d69f257e8ebf1
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-09-08 14:26:37 +1000

    bug fixes

--- models/backbone.py
+++ models/backbone.py
@@ -8,7 +8,7 @@ from dinov2.layers.block import Block, MemEffAttention
 from dinov2.models.vision_transformer import DinoVisionTransformer
 from einops import rearrange
 from torch import nn
-from mmpretrain import get_model
+# from mmpretrain import get_model
 
 from .maskclip import clip
 from .utils import block_expansion_dino
@@ -119,24 +119,24 @@ class MaskCLIP(nn.Module):
         return features
 
 
-class MOCO(nn.Module):
-    def __init__(self, name: str = "mocov3_resnet50_8xb512-amp-coslr-800e_in1k") -> None:
-        super().__init__()
+# class MOCO(nn.Module):
+#     def __init__(self, name: str = "mocov3_resnet50_8xb512-amp-coslr-800e_in1k") -> None:
+#         super().__init__()
 
-        self.model = get_model(
-            name,
-            pretrained=True,
-            data_preprocessor=dict(
-                type='SelfSupDataPreprocessor',
-                mean=(123.6750, 116.2800, 103.5300,),
-                std=(58.3950, 57.1200, 57.3750,),
-                to_rgb=True
-            )
-        )
+#         self.model = get_model(
+#             name,
+#             pretrained=True,
+#             data_preprocessor=dict(
+#                 type='SelfSupDataPreprocessor',
+#                 mean=(123.6750, 116.2800, 103.5300,),
+#                 std=(58.3950, 57.1200, 57.3750,),
+#                 to_rgb=True
+#             )
+#         )
     
-    def forward(self, x):
-        (x,) = self.model(x)
-        return rearrange(x, "B dim H W -> B (H W) dim")
+#     def forward(self, x):
+#         (x,) = self.model(x)
+#         return rearrange(x, "B dim H W -> B (H W) dim")
 
 def load_backbone(backbone_name: str) -> tuple[nn.Module, int]:
     if backbone_name == 'resnet50':
