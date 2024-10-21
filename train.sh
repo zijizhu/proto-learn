c8d9d203d74543772a03621c47e20213be080fa7
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-21 12:20:47 +1100

    Remove deprecated files

--- train.sh
+++ train.sh
@@ -1,16 +1,6 @@
 #!/bin/bash
 
 declare -a filenames=(
-# "dinov2_vitb14-base"
-# "dinov2_vitb14-fine_tune-n_splits_1"
-# "dinov2_vitb14-fine_tune"
-# "dinov2_vitb14-fine_tune-n_splits_1-l_ent"
-# "dinov2_vitb14-fine_tune-n_splits_1-l_patch_clst_sep_logits"
-# "dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits"
-# "dinov2_vitb14-fine_tune-n_splits_1-l_patch_similarity"
-# "dinov2_vitb14-fine_tune-n_splits_1"
-# "dinov2_vitb14-fine_tune"
-# "dinov2_vitb14-base"
 "dinov2_vitb14-fine_tune-n_splits_1-l_patch_clst_sep"
 "dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits-lr_decay"
 "dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits-weight_decay"
