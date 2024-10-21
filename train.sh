a29956d5b970f041a39ccef81d65334f2c513816
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-21 23:24:57 +1100

    add training configs

--- train.sh
+++ train.sh
@@ -3,10 +3,24 @@
 declare -a filenames=(
 "dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits"
 "dinov2_vitb14-fine_tune-n_splits_1-l_patch_similarity"
+"dinov2_vitb14-fine_tune-n_splits_1"
+"dinov2_vits14-fine_tune-n_splits_1"
 )
 
 for f in "${filenames[@]}"
 do
-  python train.py --base_log_dir logs-21-10 --config_path "configs/$f.yaml"
-  python evaluate.py --log_dir "logs-21-10/$f"
+  python train.py --base_log_dir logs-22-10 --config_path "configs/concept/$f.yaml"
+  python evaluate.py --log_dir "logs-22-10/concept/$f"
+done
+
+declare -a filenames=(
+"dinov2_vits14-fine_tune-n_splits_1-l_patch_logits"
+"dinov2_vits14-fine_tune-n_splits_1-l_patch_similarity"
+"dinov2_vits14-fine_tune-n_splits_1"
+)
+
+for f in "${filenames[@]}"
+do
+  python train.py --base_log_dir logs-22-10 --config_path "configs/$f.yaml"
+  python evaluate.py --log_dir "logs-22-10/$f"
 done
