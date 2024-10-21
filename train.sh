cd85a5f7982bf3a583699787f6e171c5457e0b9d
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-21 12:28:53 +1100

    update filenames

--- train.sh
+++ train.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 declare -a filenames=(
-"dinov2_vitb14-fine_tune-n_splits_1-l_patch_clst_sep"
-"dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits-lr_decay"
-"dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits-weight_decay"
-"dinov2_vitb14-fine_tune-n_splits_1-l_patch_similarity-lr_decay"
+"dinov2_vitb14-fine_tune-n_splits_1-l_patch_logits"
+"dinov2_vitb14-fine_tune-n_splits_1-l_patch_similarity"
 )
 
 for f in "${filenames[@]}"
 do
-  python train_dino.py --base_log_dir logs-16-10 --config_path "configs/$f.yaml"
-  python dino_eval.py --log_dir "logs-16-10/$f"
+  python train.py --base_log_dir logs-21-10 --config_path "configs/$f.yaml"
+  python eval.py --log_dir "logs-21-10/$f"
 done
--- train_few_shot.sh
+++ train_few_shot.sh
@@ -11,6 +11,6 @@ declare -a filenames=(
 
 for f in "${filenames[@]}"
 do
-  python train_dino.py --base_log_dir logs-09-10 --config_path "configs/few-shot/$f.yaml"
-  python dino_eval.py --log_dir "logs-09-10/$f"
+  python train.py --base_log_dir logs-09-10 --config_path "configs/few-shot/$f.yaml"
+  python eval.py --log_dir "logs-09-10/$f"
 done
